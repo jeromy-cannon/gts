9e5631c63f9e730d86677f7eefc7a4fd029402e5
Jeromy Cannon <jeromy@swirldslabs.com>
2024-11-27 22:02:08 +0000

    removed commented out sections of eslint config
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,18 +4,6 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettier from "eslint-plugin-prettier";
 import tsEslint from 'typescript-eslint';
 import typescriptParser from '@typescript-eslint/parser';
-// import tsParser from "@typescript-eslint/parser";
-// import path from "node:path";
-// import { fileURLToPath } from "node:url";
-// import { FlatCompat } from "@eslint/eslintrc";
-//
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-//   recommendedConfig: eslintJs.configs.recommended,
-//   allConfig: eslintJs.configs.all
-// });
 
 export default [
   eslintJs.configs.recommended,
