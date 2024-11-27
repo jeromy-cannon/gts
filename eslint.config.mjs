a23a9df19d3f666a362d7765659a7ac797fc2f6f
Jeromy Cannon <jeromy@swirldslabs.com>
2024-11-27 21:43:21 +0000

    first draft of upgrading to eslint 9.x
    Signed-off-by: Jeromy Cannon <jeromy@swirldslabs.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,99 @@
+import eslintJs from "@eslint/js";
+import nodePlugin from "eslint-plugin-n";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import tsEslint from 'typescript-eslint';
+import typescriptParser from '@typescript-eslint/parser';
+// import tsParser from "@typescript-eslint/parser";
+// import path from "node:path";
+// import { fileURLToPath } from "node:url";
+// import { FlatCompat } from "@eslint/eslintrc";
+//
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+//   recommendedConfig: eslintJs.configs.recommended,
+//   allConfig: eslintJs.configs.all
+// });
+
+export default [
+  eslintJs.configs.recommended,
+  nodePlugin.configs['flat/recommended'],
+  eslintConfigPrettier,
+  {
+    ignores: ['build/', 'test/fixtures/', 'template/'],
+  },
+  {
+    plugins: {
+      n: nodePlugin,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'block-scoped-var': 'error',
+      eqeqeq: 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'eol-last': 'error',
+      'prefer-arrow-callback': 'error',
+      'no-trailing-spaces': 'error',
+      quotes: ['warn', 'single', {avoidEscape: true}],
+      'no-restricted-properties': [
+        'error',
+        {
+          object: 'describe',
+          property: 'only',
+        },
+        {
+          object: 'it',
+          property: 'only',
+        },
+      ],
+    },
+  },
+  ...tsEslint.configs.recommended.map(config => ({
+    ...config,
+    files: ['**/*.ts', '**/*.tsx'],
+  })),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/no-warning-comments': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/camelcase': 'off',
+      'n/no-missing-import': 'off',
+      'n/no-empty-function': 'off',
+      'n/no-unsupported-features/es-syntax': 'off',
+      'n/no-missing-require': 'off',
+      'n/shebang': 'off',
+      'n/no-unpublished-import': [
+        'error',
+        {
+          convertPath: [
+            {
+              include: ['src/**'],
+              replace: ['^src/(.+)$', 'build/$1'],
+            },
+          ],
+        },
+      ],
+      'no-dupe-class-members': 'off',
+      'require-atomic-updates': 'off',
+    },
+  }];
